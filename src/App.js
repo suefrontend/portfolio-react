7867e7ee90351b02bb5762643edb15ef1c79ba7d
Kaya0312 <oyama_kayano@yahoo.co.jp>
2022-02-20 16:26:10 -0800

    feat: Add code for Skills page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HomePage from './views/Home/HomePage';
+import SkillsPage from './views/Skills/SkillsPage';
 
 function App() {
   return (
     <div>
       <HomePage />
+      <SkillsPage />
     </div>
   );
 }
